dbf7a150220eeb1c8bf8802bd2b2775fca48d696
Phuong Ho <phuongho43@gmail.com>
2022-03-15 00:39:04 -0700

    Add SG386 functions

--- SG386.py
+++ SG386.py
@@ -9,6 +9,7 @@ class SG386(object):
     """
     def __init__(self, serial_port):
         self.open_connection(serial_port)
+        self.max_amp = 100
 
     def open_connection(self, serial_port):
         """
@@ -22,11 +23,11 @@ class SG386(object):
             raise
 
     def close_connection(self):
-            try:
-                self.ser.close()
-            except Exception as e:
-                print(e)
-                raise
+        try:
+            self.ser.close()
+        except Exception as e:
+            print(e)
+            raise
 
     def get_identity(self):
         """
@@ -36,7 +37,202 @@ class SG386(object):
             self.ser.reset_input_buffer()
             self.ser.write(b'*IDN?\n')
             r = self.ser.readline()
-            print(r)
+            return r
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_modulation_type(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'TYPE?\n')
+            r = self.ser.readline()
+            return int(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_modulation_type(self, mtype):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'TYPE {}\n'.format(mtype))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_modulation_function(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'MFNC?\n')
+            r = self.ser.readline()
+            return int(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_modulation_function(self, mfunc):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'MFNC {}\n'.format(mfunc))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_frequency(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'FREQ?\n')
+            r = self.ser.readline()
+            return float(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_frequency(self, freq):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'FREQ {}\n'.format(freq))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_PRBS_pulse_period(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'RPER?\n')
+            r = self.ser.readline()
+            return float(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_PRBS_pulse_period(self, bit_per):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'RPER {}\n'.format(bit_per))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_PRBS_pulse_length(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'PRBS?\n')
+            r = self.ser.readline()
+            return float(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_PRBS_pulse_length(self, bit_len):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'PRBS {}\n'.format(bit_len))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_modulation_state(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'MODL?\n')
+            r = self.ser.readline()
+            return int(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_modulation_state(self, mstate):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'MODL {}\n'.format(mstate))
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_RF_amplitude(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'AMPR?\n')
+            r = self.ser.readline()
+            return float(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_RF_amplitude(self, amp):
+        try:
+            self.ser.reset_input_buffer()
+            if amp < self.max_amp:
+                self.ser.write(b'AMPR {}\n'.format(amp))
+            else:
+                print('Output amplitude too high!')
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def get_RF_state(self):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'ENBR?\n')
+            r = self.ser.readline()
+            return int(r)
+        except serial.SerialException as e:
+            print(e)
+            raise
+        except Exception as e:
+            print(e)
+            raise
+
+    def set_RF_state(self, rfstate):
+        try:
+            self.ser.reset_input_buffer()
+            self.ser.write(b'ENBR {}\n'.format(rfstate))
         except serial.SerialException as e:
             print(e)
             raise
